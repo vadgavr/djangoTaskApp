dfcb4950cc578cafffbc002b3302bfe751314dd3
Vadim <vadim@Vadims-MacBook-Pro-2.local>
2022-02-26 17:45:36 +0200

    djang task app bootstrap

---
+++ main/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render, redirect
+
+from .forms import TaskForm
+from .models import Task
+
+
+# Create your views here.
+def index(request):
+    tasks = Task.objects.order_by('-id')
+    return render(request, 'main/index.html', {'title': 'Main page', 'tasks': tasks})
+
+
+def about(request):
+    return render(request, 'main/about.html')
+
+
+def create(request):
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+        else:
+            error = 'Form was incorrect'
+
+    form = TaskForm()
+    context = {
+        'form': form,
+        'error': error
+    }
+    return render(request, 'main/create.html', context)
